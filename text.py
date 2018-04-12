cbe7e6423268f46e753b0d5e5f0cbd8b732ff5ca
DeBesciaq <ddoommiinniikk123@gmail.com>
2018-04-12 12:31:40 +0200

    1

---
+++ text.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+import asyncio
+
+
+def log(command, author_name, author_mention):
+    x = str(author_name)
+    y = str(author_mention)
+    print("------")
+    print("Komenda: "+command)
+    print("Uzytkownik: "+x+", "+y)
+    print("------")
+
+bot = commands.Bot
+
+
+while True:
+    x = str(input())
+    await bot.say(x)
